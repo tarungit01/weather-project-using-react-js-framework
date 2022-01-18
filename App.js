c6e42d132d33b2e1472090f535acc3fb01f4e8dd
Tarun Singh <67340592+tarungit01@users.noreply.github.com>
2022-01-18 21:12:39 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Temp from "./Weather/Code/temp";
+import Weathercard from "./Weather/Code/weathercard";
+
+function App() {
+  return (
+    <>
+      <Temp />
+      <Weathercard />
+    </>
+  );
+}
+
+export default App;
